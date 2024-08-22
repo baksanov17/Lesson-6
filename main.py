dd2af9861f9269afd96a4a75964b13e7695cf42d
baksanov17 <baksanov17@mail.ru>
2024-08-23 01:38:26 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+immutable_var = (1, 'ram', 1.5)
+print(immutable_var)
+#immutable_var[0] = 1, кортеж содержит неизменяемые элементы
+mutable_list = [1,2,3, 'blabla']
+mutable_list[1]= 'jojo'
+print(mutable_list)
